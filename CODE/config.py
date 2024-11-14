3592f7ba2c6c0a22a26a83c54e24ccb27b42de47
Bukanoji <144195479+Bukanoji@users.noreply.github.com>
2024-11-15 06:53:53 +0700

    Add files via upload

--- CODE/config.py
+++ CODE/config.py
@@ -1,10 +1,9 @@
-import os
 from flask_sqlalchemy import SQLAlchemy 
 from flask import Flask
 # Initialize app and database connection
 app = Flask(__name__)
 # Configuring MySQL database
-app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv ('mysql+mysqlconnector://oji:oji@localhost/oji')
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqlconnector://oji:oji@localhost/oji'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 app.app_context().push()
